ead31e4daa449b83fda8889b0e9309b2a69587d9
ay7ee <201213@astanait.edu.kz>
2023-05-21 16:08:30 +0600

    v 1.3.1 Jsession returns as JSON CORS enabled

--- src/main/java/com/example/devhouse/user_things/security/UserRegistrationSecurityConfig.java
+++ src/main/java/com/example/devhouse/user_things/security/UserRegistrationSecurityConfig.java
@@ -2,11 +2,17 @@ package com.example.devhouse.user_things.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -16,16 +22,46 @@ public class UserRegistrationSecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+
     @Bean
     public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
-        return http.httpBasic()
-                .and().cors() // Enable CORS
-                .and().csrf().disable()
+        return http
+                .httpBasic()
+                .and()
+                .cors() // Enable CORS
+                .and()
+                .cors()
+                .and()
+                .csrf().disable()
                 .authorizeRequests()
                 .requestMatchers("/api/register/**").permitAll() // Allow access to /api/register/** without authentication
                 .anyRequest().hasAuthority("USER")
-                .and().formLogin()
-                .and().build();
+                .and()
+                .formLogin()
+                .successHandler((request, response, authentication) -> {
+                    response.setContentType("application/json");
+                    response.getWriter().write("{\"sessionId\": \"" + request.getSession().getId() + "\"}");
+                })
+                .failureHandler((request, response, exception) -> {
+                    response.setStatus(HttpStatus.UNAUTHORIZED.value());
+                    response.setContentType("application/json");
+                    response.getWriter().write("{\"error\": \"Authentication failed\"}");
+                })
+                .permitAll()
+                .and()
+                .build();
     }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*")); // Allow requests from any origin
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
 
 }
