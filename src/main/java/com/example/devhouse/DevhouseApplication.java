13127c648a587b02042dada8fd1f1944894c531f
payrlch <tolebayarsen@gmail.com>
2024-02-28 21:18:02 +0600

    new version for AF

--- src/main/java/com/example/devhouse/DevhouseApplication.java
+++ src/main/java/com/example/devhouse/DevhouseApplication.java
@@ -1,14 +1,8 @@
 package com.example.devhouse;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.spring.autoconfigure.sql.*;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import javax.sql.DataSource;
-import java.io.IOException;
-
 @SpringBootApplication
 public class DevhouseApplication {
 
